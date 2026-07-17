299df9b01e7c8a7a82249edeff356ecdfc086d4f
vajramg <vajramg@gmail.com>
2026-07-17 18:50:17 +0530

    Updated terraformfiles

--- terraform/modules/acr/main.tf
+++ terraform/modules/acr/main.tf
@@ -1,6 +1,6 @@
 
 resource "azurerm_container_registry" "acr" {
-  name                = "vg1988acrmy"
+  name                = "acrvg1988"
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = "Basic"
